8af58f1d62bb95f8ed49115abc559911a1f891c3
Chitty Liu <d2921118@gmail.com>
2018-08-17 12:19:05 +0100

    home page linked

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root to: 'pages#home'
+  root to: "cocktails#index"
   resources :cocktails do
     resources :ingredients, only: [:show]
     resources :doses, only: [:new, :create, :destroy]
